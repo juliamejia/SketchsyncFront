351c1a27fa744b99cc30761dcde02e6ee8558a10
julia <julia.mejia@mail.escuelaing.edu.co>
2023-12-07 21:36:12 -0500

    no funcionó

--- src/main/resources/static/JS/controlUsers.js
+++ src/main/resources/static/JS/controlUsers.js
@@ -1,12 +1,22 @@
-async function signIn() {
-    const name = prompt("Ingrese su nombre de usuario:"); // Pide al usuario su nombre
-    if (name) { // Comprueba si el usuario proporcionó un nombre
-        localStorage.setItem("logeo", name);
-
-        if (name === "Organizer") {
-            app.createOrganizer(name);
-        } else {
-            app.createUser(name);
+async function signIn(){
+    const config = {
+        auth: {
+            clientId: '2900126f-1811-46f9-91f1-03e9e383bd94',
+            authority: 'https://login.microsoftonline.com/common/',
+            redirectUri: 'https://sketchsync.azurewebsites.net/index.html'
         }
-    } else {
-        console.log("El usuario no proporcionó un nombre.");
+    };
+    var client = new Msal.UserAgentApplication(config);
+    var request = {
+        scopes: ['user.read']
+    };
+    let loginResponse = await client.loginPopup(request);
+    console.log("hola bienvenido ")
+    localStorage.setItem("logeo", loginResponse.account.name)
+    if (loginResponse.account.name === "Organizer"){
+        app.createOrganizer(loginResponse.account.name)
+    }else{
+
+        app.createUser(loginResponse.account.userName)
+    }
+}
